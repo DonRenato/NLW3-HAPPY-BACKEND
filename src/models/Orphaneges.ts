c176bf9a0143e4f8e4fdd3daf629f150c791a902
Renato  Avellar <onizuka@Mac-mini.local>
2020-10-14 12:12:43 -0300

    criando models

--- src/models/Orphaneges.ts
+++ src/models/Orphaneges.ts
@@ -1,9 +1,28 @@
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity('orphanages')
 export default class Orphanages{
+    @PrimaryGeneratedColumn('increment')
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     latitude: number;
+
+    @Column()
     longitude: number;
+
+    @Column()
     about: string;
+
+    @Column()
     instructions: string;
-    open_
+
+    @Column()
+    opening_hours: string;
+
+    @Column()
+    open_on_weekends: string
 }
