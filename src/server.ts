93a230d9abae4da94c055d2ca38c7271470a1d18
Renato  Avellar <onizuka@Mac-mini.local>
2020-10-14 23:39:46 -0300

    tratando erros

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
 import path from 'path';
+import errorHandler from './errors/handler';
 
 
 const app = express();
@@ -9,6 +11,6 @@ const app = express();
 app.use(express.json())
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-
+app.use(errorHandler);
 
 app.listen(3333);
